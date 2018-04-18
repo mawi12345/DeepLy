01080be700ae4762825d4738f51bca71d415b803
Martin Wind <martin.wind@im-c.at>
2018-04-18 11:36:03 +0200

    move api_key to query

--- src/ChrisKonnertz/DeepLy/HttpClient/GuzzleHttpClient.php
+++ src/ChrisKonnertz/DeepLy/HttpClient/GuzzleHttpClient.php
@@ -57,19 +57,15 @@ class GuzzleHttpClient extends CurlHttpClient
         if (! is_string($method)) {
             throw new \InvalidArgumentException('$method has to be a string');
         }
-        
-        // TODO Assuming that the API key can be added to the payload this way.
-        // This is just assumption and needs to be verified.
-        $payload['auth_key'] = $apiKey;
 
         $jsonData = $this->protocol->createRequestData($payload, $method);
 
-        $options = ['body' => $jsonData];
+        $options = ['body' => $jsonData, 'query' => ['auth_key' => $apiKey]];
 
         try {
             $guzzleResponse = $this->guzzle->post($url, $options);
         } catch (GuzzleException $exception) {
-            $callException = new CallException('cURL error during DeepLy API call: '.$exception->getMessage());
+            $callException = new CallException('Guzzle error during DeepLy API call: '.$exception->getMessage());
             throw $callException;
         }
 
